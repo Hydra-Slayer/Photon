001ada125cb70511f223374bac88bd164a5004ef
Hydra-Slayer <shreyassandeep.kodte2021@vitstudent.ac.in>
2025-07-09 19:40:21 +0530

    added dark mode toggle. -need fix collection name alignment

--- src/main/java/com/app/photon/ThumbnailLoader.java
+++ src/main/java/com/app/photon/ThumbnailLoader.java
@@ -80,7 +80,9 @@ public class ThumbnailLoader {
                 Label label = new Label(item.getName());
                 VBox vbox = new VBox(5, imageView, label);
                 vbox.setPadding(new Insets(5));
-                vbox.setStyle("-fx-alignment: center; -fx-background-color: #f0f0f0; -fx-border-color: #cccccc;");
+                // // vbox.setStyle("-fx-alignment: center; -fx-background-color: #f0f0f0;
+                // -fx-border-color: #cccccc;");
+                vbox.setStyle("-fx-alignment: center");
                 vbox.setOnMouseClicked(e -> onCollectionClick.accept(item.getName()));
 
                 gridPane.add(vbox, column, row);
